2b0baef2419ce3e00cb8a841faddc2273bcc07dd
kantharuban <Kantharuban23@gmail.com>
2014-06-19 14:43:19 +0800

    adding laravelapp.rb

--- laravel/recipes/appsetup.rb
+++ laravel/recipes/appsetup.rb
@@ -9,34 +9,6 @@ node[:deploy].each do |app_name, deploy|
     chmod -R 777 storage
     EOH
   end
-
-    template "#{deploy[:deploy_to]}/current/app/config/database.php" do
-      source "database.php.erb"
-      mode 0660
-      group deploy[:group]
-
-      if platform?("ubuntu")
-        owner "www-data"
-      elsif platform?("amazon")   
-        owner "apache"
-      end
-
-      variables(
-        :host =>     (deploy[:database][:host] rescue nil),
-        :user =>     (deploy[:database][:username] rescue nil),
-        :password => (deploy[:database][:password] rescue nil),
-        :db =>       (deploy[:database][:database] rescue nil)
-      )
-
-     only_if do
-       File.directory?("#{deploy[:deploy_to]}/current")
-     end
-    end
-
-   
-
-    
-
     
   end
 end
