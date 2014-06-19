84540456d6c3b29b03ad83ef27357549ed8e4ea8
kantharuban <Kantharuban23@gmail.com>
2014-06-19 11:54:16 +0800

    adding laravelapp.rb

--- laravel/recipes/laravelapp.rb
+++ laravel/recipes/laravelapp.rb
@@ -33,8 +33,7 @@ node[:deploy].each do |app_name, deploy|
       :host =>     (deploy[:database][:host] rescue nil),
       :user =>     (deploy[:database][:username] rescue nil),
       :password => (deploy[:database][:password] rescue nil),
-      :db =>       (deploy[:database][:database] rescue nil),
-      :table =>    (node[:phpapp][:dbtable] rescue nil)
+      :db =>       (deploy[:database][:database] rescue nil)
     )
 
    only_if do
