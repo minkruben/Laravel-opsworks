52407232c921ea2fe8046047556fd2e51a6f1d44
kantharuban <Kantharuban23@gmail.com>
2014-06-19 11:51:22 +0800

    adding laravelapp.rb

---
+++ laravel/recipes/laravelapp.rb
@@ -0,0 +1,44 @@
+node[:deploy].each do |app_name, deploy|
+  script "install_composer" do
+    interpreter "bash"
+    user "root"
+    cwd "#{deploy[:deploy_to]}/current"
+    code <<-EOH
+    curl -s https://getcomposer.org/installer | php
+    php composer.phar install
+    EOH
+  end
+
+  script "set_permissions" do
+    interpreter "bash"
+    user "root"
+    cwd "#{deploy[:deploy_to]}/current/app"
+    code <<-EOH
+    chmod -R 777 storage
+    EOH
+  end
+
+  template "#{deploy[:deploy_to]}/current/app/config/database.php" do
+    source "database.php.erb
+    mode 0660
+    group deploy[:group]
+
+    if platform?("ubuntu")
+      owner "www-data"
+    elsif platform?("amazon")   
+      owner "apache"
+    end
+
+    variables(
+      :host =>     (deploy[:database][:host] rescue nil),
+      :user =>     (deploy[:database][:username] rescue nil),
+      :password => (deploy[:database][:password] rescue nil),
+      :db =>       (deploy[:database][:database] rescue nil),
+      :table =>    (node[:phpapp][:dbtable] rescue nil)
+    )
+
+   only_if do
+     File.directory?("#{deploy[:deploy_to]}/current")
+   end
+  end
+end
