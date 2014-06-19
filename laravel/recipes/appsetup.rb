95fa9e17fc8a5dd5c46e154c191227aca824642d
kantharuban <Kantharuban23@gmail.com>
2014-06-19 11:25:15 +0800

    adding recipes

---
+++ laravel/recipes/appsetup.rb
@@ -0,0 +1,42 @@
+node[:deploy].each do |app_name, deploy|
+
+  if deploy[:application] == "platform"
+   script "set_permissions" do
+    interpreter "bash"
+    user "root"
+    cwd "#{deploy[:deploy_to]}/current/app"
+    code <<-EOH
+    chmod -R 777 storage
+    EOH
+  end
+
+    template "#{deploy[:deploy_to]}/current/app/config/database.php" do
+      source "database.php.erb"
+      mode 0660
+      group deploy[:group]
+
+      if platform?("ubuntu")
+        owner "www-data"
+      elsif platform?("amazon")   
+        owner "apache"
+      end
+
+      variables(
+        :host =>     (deploy[:database][:host] rescue nil),
+        :user =>     (deploy[:database][:username] rescue nil),
+        :password => (deploy[:database][:password] rescue nil),
+        :db =>       (deploy[:database][:database] rescue nil)
+      )
+
+     only_if do
+       File.directory?("#{deploy[:deploy_to]}/current")
+     end
+    end
+
+   
+
+    
+
+    
+  end
+end
