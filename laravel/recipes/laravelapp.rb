e6607e26d1801db0d8ecb41c6c07b43beae49d6d
kantharuban <Kantharuban23@gmail.com>
2014-06-19 14:40:09 +0800

    adding laravelapp.rb

--- laravel/recipes/laravelapp.rb
+++ laravel/recipes/laravelapp.rb
@@ -2,7 +2,7 @@ node[:deploy].each do |app_name, deploy|
   script "install_composer" do
     interpreter "bash"
     user "root"
-    cwd "#{deploy[:deploy_to]}/current"
+    cwd "#{deploy[:deploy_to]}/current/vendor/laravel/framework/"
     code <<-EOH
     curl -s https://getcomposer.org/installer | php
     php composer.phar install
